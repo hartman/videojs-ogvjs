042b548d9516da3d664f036db1a6c68989c76db0
Derk-Jan Hartman <hartman.wiki@gmail.com>
2016-10-07 12:50:02 +0200

    Update to new plugin structure
    Also fix karma testcases
    Upgrade videojs and ogv.js versions

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,10 +1,10 @@
-// import document from 'global/document';
+import document from 'global/document';
 
 import QUnit from 'qunit';
 import sinon from 'sinon';
 import videojs from 'video.js';
 
-// import plugin from '../src/plugin';
+import plugin from '../src/plugin';
 
 // const Player = videojs.getComponent('Player');
 
@@ -12,47 +12,62 @@ QUnit.test('the environment is sane', function(assert) {
   assert.strictEqual(typeof Array.isArray, 'function', 'es5 exists');
   assert.strictEqual(typeof sinon, 'object', 'sinon exists');
   assert.strictEqual(typeof videojs, 'function', 'videojs exists');
+  assert.strictEqual(typeof plugin, 'function', 'plugin is a function');
 });
 
-// // QUnit.module('videojs-ogvjs', {
+QUnit.module('videojs-ogvjs', {
 
-// //   beforeEach() {
-// //     this.fixture = document.getElementById('qunit-fixture');
-// //     this.video = document.createElement('video');
-// //     this.fixture.appendChild(this.video);
-// //     this.player = new Player();
+  beforeEach() {
 
-// //     // Mock the environment's timers because certain things - particularly
-// //     // player readiness - are asynchronous in video.js 5.
-// //     this.clock = sinon.useFakeTimers();
-// //   },
+    // Mock the environment's timers because certain things - particularly
+    // player readiness - are asynchronous in video.js 5. This MUST come
+    // before any player is created; otherwise, timers could get created
+    // with the actual timer methods!
+    this.clock = sinon.useFakeTimers();
 
-// //   afterEach() {
-// //     this.player.dispose();
-// //     this.clock.restore();
-// //   }
-// // });
+    this.fixture = document.getElementById('qunit-fixture');
+    this.video = document.createElement('video');
+    this.source = document.createElement('source');
+    this.source.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/9/94/Folgers.ogv');
+    this.source.setAttribute('type', 'video/ogg');
+    this.video.appendChild(this.source);
+    this.fixture.appendChild(this.video);
+    this.player = videojs(this.video, {
+      techOrder: [ 'ogvjs' ],
+      ogvjs: { base: './node_modules/ogv/dist' }
+    });
+  },
 
-// QUnit.test('registers itself with video.js', function(assert) {
-//   assert.ok(
-//     typeof videojs.getTech('Ogvjs'),
-//     'function',
-//     'videojs-ogvjs plugin is a function'
-//   );
+  afterEach() {
+    this.player.dispose();
+    this.clock.restore();
+  }
+});
 
-//   assert.strictEqual(
-//     Player.prototype.ogvjs,
-//     plugin,
-//     'videojs-ogvjs plugin was registered'
-//   );
+QUnit.test('registers itself with video.js', function(assert) {
+  assert.expect(3);
 
-//   this.player.ogvjs();
+  // this.player.test();
 
-//   // Tick the clock forward enough to trigger the player to be "ready".
-//   this.clock.tick(1);
+  // Tick the clock forward enough to trigger the player to be "ready".
+  this.clock.tick(1);
 
-//   assert.ok(
-//     this.player.hasClass('vjs-ogvjs'),
-//     'the plugin adds a class to the player'
-//   );
-// });
+  assert.ok(
+    typeof videojs.getTech('Ogvjs'),
+    'function',
+    'videojs-ogvjs plugin is a function'
+  );
+
+  assert.ok(
+    videojs.hasClass(
+      this.player.tech({ IWillNotUseThisInPlugins: true}).el(),
+      'vjs-tech'
+    ),
+    'the plugin adds a class to the player'
+  );
+
+  assert.ok(
+    this.player.tech({ IWillNotUseThisInPlugins: true}).el().tagName === 'OGVJS',
+    'the plugin adds an ogvjs element'
+  );
+});
